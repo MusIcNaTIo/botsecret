5b53d8f6269382a0bb5382a2ef00d99a32900938
Guysmow <41370451+Guysmow@users.noreply.github.com>
2018-08-06 17:30:34 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -992,99 +992,7 @@ if(message.content.startsWith(prefix + "report")) {
      message.channel.send("Votre message a été envoyé au staff du bot !")
  
  }
- if(message.content === prefix + "slot") {
-  var replys1 = [
-":dollar:|:dollar:|:dollar:",
-":cherries:|:cherries:|:cherries:",
-":seven:|:seven:|:seven: ",
-":large_orange_diamond:|:large_orange_diamond:|:large_orange_diamond:",
-":large_blue_circle:|:large_blue_circle:|:large_blue_circle:",
-":x:|:x:|:x:",
-":tangerine:|:tangerine:|:tangerine:",
-":dollar:|:cherries:|:dollar:|",
-":dollar:|:cherries:|:large_orange_diamond:",
-":cherries:|:tangerine:|:tangerine:",
-":x:|:seven:|:cherries:",
-":seven:|:dollar:|:tangerine:",
-":cherries:|:large_blue_circle:|:tangerine:",
-":large_blue_circle:|:large_blue_circle:|:cherries:",
-":x:|:cherries:|:cherries:",
-":dollar:|:dollar:|:seven:",
-":large_orange_diamond:|:x:|:x:",
-":seven:|:tangerine:|:tangerine:",
-":large_orange_diamond:|:large_orange_diamond:|:x:",
-":x:|:x:|:seven:",
-":large_blue_circle:|:large_orange_diamond:|:seven:",
-":cherries:|:cherries:|:x:",
-":tangerine:|:cherries:|:x:",
 
-  ];
-   let reponse = (replys1[Math.floor(Math.random() * replys1.length)])
-   
-      var replys2 = [
-        ":dollar:|:dollar:|:dollar:",
-":cherries:|:cherries:|:cherries:",
-":seven:|:seven:|:seven: ",
-":large_orange_diamond:|:large_orange_diamond:|:large_orange_diamond:",
-":large_blue_circle:|:large_blue_circle:|:large_blue_circle:",
-":x:|:x:|:x:",
-":tangerine:|:tangerine:|:tangerine:",
-":dollar:|:cherries:|:dollar:|",
-":dollar:|:cherries:|:large_orange_diamond:",
-":cherries:|:tangerine:|:tangerine:",
-":x:|:seven:|:cherries:",
-":seven:|:dollar:|:tangerine:",
-":cherries:|:large_blue_circle:|:tangerine:",
-":large_blue_circle:|:large_blue_circle:|:cherries:",
-":x:|:cherries:|:cherries:",
-":dollar:|:dollar:|:seven:",
-":large_orange_diamond:|:x:|:x:",
-":seven:|:tangerine:|:tangerine:",
-":large_orange_diamond:|:large_orange_diamond:|:x:",
-":x:|:x:|:seven:",
-":large_blue_circle:|:large_orange_diamond:|:seven:",
-":cherries:|:cherries:|:x:",
-":tangerine:|:cherries:|:x:",
-   ];
-   let reponse2 = (replys2[Math.floor(Math.random() * replys2.length)])
-          var replys3 = [
-            ":dollar:|:dollar:|:dollar:",
-":cherries:|:cherries:|:cherries:",
-":seven:|:seven:|:seven: ",
-":large_orange_diamond:|:large_orange_diamond:|:large_orange_diamond:",
-":large_blue_circle:|:large_blue_circle:|:large_blue_circle:",
-":x:|:x:|:x:",
-":tangerine:|:tangerine:|:tangerine:",
-":dollar:|:cherries:|:dollar:|",
-":dollar:|:cherries:|:large_orange_diamond:",
-":cherries:|:tangerine:|:tangerine:",
-":x:|:seven:|:cherries:",
-":seven:|:dollar:|:tangerine:",
-":cherries:|:large_blue_circle:|:tangerine:",
-":large_blue_circle:|:large_blue_circle:|:cherries:",
-":x:|:cherries:|:cherries:",
-":dollar:|:dollar:|:seven:",
-":large_orange_diamond:|:x:|:x:",
-":seven:|:tangerine:|:tangerine:",
-":large_orange_diamond:|:large_orange_diamond:|:x:",
-":x:|:x:|:seven:",
-":large_blue_circle:|:large_orange_diamond:|:seven:",
-":cherries:|:cherries:|:x:",
-":tangerine:|:cherries:|:x:",
-   ];
-   let reponse3 = (replys3[Math.floor(Math.random() * replys3.length)])
-
-   const embed = new Discord.RichEmbed()
-   .setColor("#FE0101")
-   .setTitle(`**[ :slot_machine: @${message.author.tag} a lancé la machine à sous! :slot_machine: ]**`)
-   .addField("**-------------------**", "** **")
-   .addField(`${reponse} \n \n${reponse2}**<--** \n \n${reponse3}`, `** **`)
-   .addField("**-------------------**", "** **")
-   .setDescription("** **")
-   .setFooter("Slot")
-   message.channel.send(embed)
-   console.log("J'ai lancé la machine à sous!")
- }
 
  if(message.content.startsWith(prefix + "avatar")) {
 	let args = message.content.split(" ").slice(1);
@@ -1327,6 +1235,7 @@ if(!sondage) return message.reply("Entrez un texte")
 console.log(`Un sondage a été envoyé correctement et son sujet est : ${sondage}`)
 }
 });
+
 
 
 
