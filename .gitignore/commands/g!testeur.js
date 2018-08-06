434549a4c772916a61cb260ee67ec4f255619437
Guysmow <41370451+Guysmow@users.noreply.github.com>
2018-08-06 22:26:03 +0200

    Create g!testeur.js

---
+++ .gitignore/commands/g!testeur.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js"); // Discord Module Required
+exports.run = async (client, message, args1) => {
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+message.channel.send(`${message.author} a besoin de vous ! @everyone`)
+
+}
